e8a64abbcfd8957afcaa017cdad898165db0c1ae
Simon Tye <simontye@uark.edu>
2020-04-20 15:10:05 -0500

    homework and class notes

--- Assignments/parseGFF.py
+++ Assignments/parseGFF.py
@@ -16,35 +16,77 @@
 # Import modules
 import argparse
 import csv
+import re
 from Bio import SeqIO
 
-# Create argument parser object
-parser = argparse.ArgumentParser(description = "This script parses a GFF file")
+# Function to parse command line
+def get_args():
 
-# Add positional arguments
-parser.add_argument("gff",   help = "Name of the GFF file")
-parser.add_argument("fasta", help = "Name of the FASTA file")
+	# Create argument parser object
+	parser = argparse.ArgumentParser(description = "This script parses a GFF file")
 
-# Parse the arguments
-args = parser.parse_args()
+	# Add positional arguments
+	parser.add_argument("gff",   help = "Name of the GFF file")
+	parser.add_argument("fasta", help = "Name of the FASTA file")
 
-# Read and parse the fasta file
-genome = SeqIO.read(args.fasta, "fasta")
-genome_seq = genome.seq
+	# Parse the arguments
+	return parser.parse_args()
 
-# Create function
-with open(args.gff, "r") as gff_file:
+# Function to parse fasta
+def parse_fasta():
+	return SeqIO.read(args.fasta, "fasta")
+
+# Function to parse gff
+def parse_gff:():
+	with open(args.gff, "r") as gff_file:
     reader = csv.reader(gff_file, delimiter = "\t")
     for line in reader:
-        if line[2] != "CDS":
+        if line[2] == "CDS":
+        
             # Define start and end positions
-            start = line[3]
-            end   = line[4]
-            # Find substrings in genome_seq
-            substring = genome_seq[(start - 1):(end - 1)]
-            # Calculate GC content
-            g_count = substring.count("G")
-            c_count = substring.count("C")
-            length  = len(substring)
-            # Print output
-            print((g_count + c_count)/length)
+            organism   = line[0].replace(" ", "_")
+            start      = int(line[3])
+            end        = int(line[4])
+            strand     = line[6]
+            attributes = line[8]
+            
+            # Print rounded output
+            feature_gc = gc([start-1:end])
+            
+            # Extract gene name
+            match = re.search("Gene\s+(\S+)\s", attributes)
+            gene_name = match.group(1)
+            
+            # print FASTA format
+            print(">" + organism + "_" + gene_name)
+            print(feature_seq)
+            # print(attributes)
+            # print(feature_gc)
+            # gc = (g_count + c_count)/len(substring))
+            # gc_round = round(gc, 2)
+            # print (gc_round)
+            # print("{0:.2f}".format (GC))
+	
+# Function to calculate GC content
+def gc(sequence):
+	g_count = sequence.count("G")
+    c_count = sequence.count("C")
+	gc = (g_count + c_count)/len(sequence))
+	gc_round = round(gc, 2)
+
+# Last function to use all of these things
+def main():
+	genome_sequence = parse_fasta()
+	parse_gff(genome_sequence.seq)
+	
+# Call get_args before calling main
+args = get_args()
+	
+# Execute the program by calling main
+if __name__ == "__main__":
+	main()
+
+
+
+
+
